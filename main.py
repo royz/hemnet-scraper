01acb495ca3254eb7b65b0180145daf9ad035fbb
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-10-14 00:14:46 +0530

    added search function and function to refersh the tokens

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
-import config
 import requests
 import time
+import json
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
+             '(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
 
 
 class Faktakontroll:
     def __init__(self):
-        self.refresh_token = config.refresh_token
+        self.refresh_token = None
+        self.access_token = None
+        self.read_config()
+
+    def read_config(self):
+        with open('config.json') as f:
+            tokens = json.load(f)
+            self.refresh_token = tokens['refreshToken']
+            self.access_token = tokens['accessToken']
+
+    def write_config(self):
+        tokens = {
+            'refreshToken': self.refresh_token,
+            'accessToken': self.access_token
+        }
+
+        with open('config.json', 'w', encoding='utf-8') as f:
+            json.dump(tokens, f, indent=2)
+
+    def refresh_tokens(self):
+        cookies = {
+            'ext_name': 'ojplmecpdpgccookcobabopnaifgidhf',
+            'user': 'true',
+        }
+
+        headers = {
+            'Connection': 'keep-alive',
+            'Accept': 'application/json, text/plain, */*',
+            'X-Initialized-At': str(int(time.time() * 1000)),
+            'DNT': '1',
+            'User-Agent': USER_AGENT,
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Origin': 'https://www.faktakontroll.se',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://www.faktakontroll.se/app/sok',
+            'Accept-Language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+        }
+
+        data = f'"{self.refresh_token}"'
+
+        try:
+            response = requests.post('https://www.faktakontroll.se/app/api/auth/refresh', headers=headers,
+                                     cookies=cookies,
+                                     data=data)
+            tokens = response.json()
+            self.refresh_token = tokens['refreshToken']
+            self.access_token = tokens['accessToken']
+            self.write_config()
+            print(tokens)
+        except Exception as e:
+            print(f'error while refreshing tokens: {e}')
 
     def search(self):
         cookies = {
@@ -16,11 +71,9 @@ class Faktakontroll:
         headers = {
             'Connection': 'keep-alive',
             'Accept': 'application/json, text/plain, */*',
-            'X-Initialized-At': int(time.time() * 1000),
-            'X-Auth-Token': config.access_token,
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/86.0.4240.75 Safari/537.36',
+            'X-Initialized-At': str(int(time.time() * 1000)),
+            'X-Auth-Token': self.access_token,
+            'User-Agent': USER_AGENT,
             'DNT': '1',
             'Content-Type': 'application/json;charset=UTF-8',
             'Origin': 'https://www.faktakontroll.se',
@@ -31,7 +84,17 @@ class Faktakontroll:
             'Accept-Language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
         }
 
-        data = '{"searchString":"Florav\xE4gen 1, Nyn\xE4shamn","filterType":"","subscriptionRefNo":"20.750.025.01"}'
+        data = {"searchString": "Florav\xE4gen 1, Nyn\xE4shamn", "filterType": "", "subscriptionRefNo": "20.750.025.01"}
 
         response = requests.post('https://www.faktakontroll.se/app/api/search', headers=headers, cookies=cookies,
-                                 data=data)
+                                 json=data)
+
+        # status code 500 when access token expires
+        print(response)
+        with open('resp.json', 'w', encoding='utf-8') as f:
+            json.dump(response.json(), f, indent=2)
+
+
+if __name__ == '__main__':
+    f = Faktakontroll()
+    f.search()
